3acfe0c9488212c50424b5f664d45e78bda29c96
pgilley-coursera <pgilley@coursera.org>
2025-08-25 13:21:34 -0600

    Switch from demo app to full app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-// import App from './App';  // Comment out for demo
-import AppDeploy from './AppDeploy';  // Use deployment demo app
+import App from './App';  // Use the full app
+// import AppDeploy from './AppDeploy';  // Comment out or remove the demo version
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppDeploy />
+    <App />
   </React.StrictMode>
 );
 reportWebVitals();
